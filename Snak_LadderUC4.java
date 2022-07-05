a6cfe741697cb582f4e32d7da7c82e4ceb73919f
ashutoshnawale89 <ashutoshnawale89@gmail.com>
2022-07-05 17:39:53 +0530

    UC4 ADDED

---
+++ Snak_LadderUC4.java
@@ -0,0 +1,44 @@
+public class Snak_LadderUC4 {
+	// Constant
+	public static final  int NO_PLAY=1;
+	public static final int LADDER_PLAY=2;
+	public static final	int SNAK_PLAY=3;
+	public static final int Ladder_board=100;
+	
+	public static void main(String[] args) {
+		System.out.println("WLCOME TO SNAKE LADDER GAME");
+		// Variables
+		
+		int diesrolled;
+		int newposition=0;
+		int player1_position=0; 
+		
+	while (player1_position <= Ladder_board )
+	{
+	diesrolled = (int) (Math.random()*6) + 1;
+	System.out.println("DIES ROLLED" + diesrolled );
+	int instruction = (int) (Math.random()*3+1);
+	System.out.println("instruction" + instruction );
+	
+	switch ((int)instruction )  {
+	case  NO_PLAY :
+		newposition=diesrolled -(1 * diesrolled) ;
+       break;
+  case LADDER_PLAY :
+	  newposition= (1 * diesrolled);
+		break;
+  case SNAK_PLAY :
+	  newposition= -(1 * diesrolled) ;
+	}
+	    player1_position = player1_position + newposition ;
+	  System.out.println("The value of instruction is  " + player1_position );
+	 
+		 
+	  }
+	  System.out.println("Player 1 Now @ " + player1_position + "the" + "Position");
+	  
+
+	 	 
+	System.out.println("###########-------------  THE PLAYER 1 IS WON ------------#########  ");
+}     
+}
