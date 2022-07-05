c80dbbc13f841c3c573c0aaf24086d851ac706cf
ashutoshnawale89 <ashutoshnawale89@gmail.com>
2022-07-05 17:35:35 +0530

    UC2 ADDED

--- Snak_LadderUC2.java
+++ Snak_LadderUC2.java
@@ -1,11 +1,11 @@
 
-public class Snak_LadderUC1 {
+public class Snak_LadderUC2 {
 	public static void main(String[] args) {
 		
 
 		int dies;
-		// player role die
-	dies = (int) (Math.random()*0) ;
+		// result
+	dies = (int) (Math.random()*6) + 1;
 	
 		System.out.println("the dice number is" + dies  );
 
